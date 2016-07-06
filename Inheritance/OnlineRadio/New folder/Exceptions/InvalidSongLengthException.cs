aba2facdd754147df6dd276c2f1db5dd9aea45e0
thrusanova <thrusanova@gmail.com>
2016-07-06 19:59:54 +0300

    inheritance

---
+++ Inheritance/OnlineRadio/New folder/Exceptions/InvalidSongLengthException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineRadio.Exceptions
+{
+   public class InvalidSongLengthException:InvalidSongException
+    { 
+        public new const string Message = "Invalid song length.";
+        public InvalidSongLengthException() : base(Message)
+        {
+        }
+        public InvalidSongLengthException(string message) : base(message)
+        {
+        }
+    }
+}
