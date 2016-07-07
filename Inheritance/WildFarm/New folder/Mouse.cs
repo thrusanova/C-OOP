3847002c333eba536a5808128c33377789d6a35a
thrusanova <thrusanova@gmail.com>
2016-07-07 07:46:30 +0300

    polymorphism@

---
+++ Inheritance/WildFarm/New folder/Mouse.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WildFarm
+{
+    public class Mouse : Mammal
+    {
+        public Mouse(string animalName, string animalType, double animalWeight, string region)
+            : base(animalName, animalType, animalWeight, region)
+        {
+        }
+
+        public override void Eat(Food food)
+        {
+            if (food is Meat)
+            {
+                Console.WriteLine($"{this.AnimalType}s are not eating that type of food!");
+            }
+            else
+            {
+                Console.WriteLine("A cheese was just eaten!");
+                base.FoodEaten += food.quantity;
+            }
+
+        }
+        public override void MakeSound()
+        {
+            Console.WriteLine("SQUEEEAAAK!");
+        }
+    }
+}
---
+++ Inheritance/WildFarm/New folder/Zebra.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WildFarm
+{
+    public class Zebra : Mammal
+    {
+        public Zebra(string animalName, string animalType, double animalWeight, string livingRegion) : base(animalName, animalType, animalWeight, livingRegion)
+        {
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("Zs");
+        }
+    }
+}
