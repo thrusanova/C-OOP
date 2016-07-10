8ac9a77aeb3393d114d11caf57f3a6dcbd7c671f
thrusanova <thrusanova@gmail.com>
2016-07-10 13:01:15 +0300

    commit sample exam

---
+++ SampleExam/SExam/New folder/Item/Device.cs
@@ -0,0 +1,25 @@
+﻿
+namespace SExam.Item
+{
+   public class Device
+    {
+        private decimal deviceCost;
+
+        public Device(decimal deviceCost)
+        {
+            this.DeviceCost = deviceCost;
+        }
+
+        public decimal DeviceCost
+        {
+            get
+            {
+                return this.deviceCost;
+            }
+            private set
+            {
+                this.deviceCost = value;
+            }
+        }
+    }
+}
