f6213b992d6044637e26f3eea35dc463ecb4838f
thrusanova <thrusanova@gmail.com>
2017-01-15 14:27:55 +0200

    Exam

---
+++ Dealership/Models/Car.cs
@@ -0,0 +1,37 @@
+﻿
+namespace Dealership.Models
+{
+    using Contracts;
+    using System;
+    using System.Collections.Generic;
+    using Common.Enums;
+    using System.Text;
+    using Common;
+    public class Car :Vehicle, IVehicle,ICar
+    {
+        private int seats;
+
+        public Car(string make, string model, decimal price,int seats) : base(make,model,price,VehicleType.Car)
+        { 
+            this.seats = seats;
+            ValidateSeats();
+        }
+
+        public int Seats { get; private set; }
+
+        private void ValidateSeats()
+        {
+            Validator.ValidateIntRange(this.seats, Constants.MinSeats, Constants.MaxSeats, string.Format(Constants.NumberMustBeBetweenMinAndMax, "Seats",Constants.MinSeats,Constants.MaxSeats));
+        }
+        public override string ToString()
+
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(base.ToString());
+            sb.Append(string.Format("  Seats: {0}", this.seats));
+            return sb.ToString();
+        }
+
+
+    }
+}
---
+++ Dealership/Models/Truck.cs
@@ -0,0 +1,45 @@
+﻿
+namespace Dealership.Models
+{
+    using System.Text;
+    using Common.Enums;
+    using Contracts;
+    using Common;
+    public class Truck : Vehicle,IVehicle,ITruck
+    {
+        private int weightCapacity; 
+
+        public Truck(string make, string model, decimal price,int weightCapacity) : base(make, model, price, VehicleType.Truck)
+        {
+            this.WeightCapacity = weightCapacity;
+            ValidateCapacity();
+        }
+
+        public int WeightCapacity
+        {
+            get
+            {
+                return this.weightCapacity;
+            }
+            private set
+            {
+                this.weightCapacity = value;
+            }
+        }
+
+
+        private void ValidateCapacity()
+        {
+            Validator.ValidateIntRange(this.weightCapacity, Constants.MinCapacity, Constants.MaxCapacity, string.Format(Constants.NumberMustBeBetweenMinAndMax, "Weight capacity",Constants.MinCapacity,Constants.MaxCapacity));
+        }
+        public override string ToString()
+
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(base.ToString());
+            sb.Append(string.Format("  Weight capacity: {0}", this.WeightCapacity));
+            return sb.ToString();
+        }
+
+    }
+}
